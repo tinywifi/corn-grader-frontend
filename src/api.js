0a3a2760b6a4386c8ba9cdf9580b1656d2b7df99
tinywifi <tinywifi@pm.me>
2025-06-08 10:41:56 +0700

    trying to use new api with cors

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 const API_URL = import.meta.env.VITE_API_URL || 'https://corn-grader.onrender.com'
 
-export const uploadImage = async (formData) => {
-  const res = await fetch(`${API_URL}/`, {
-    method: 'POST',
+export async function uploadImage(formData) {
+  const response = await fetch(API_URL, {
+    method: "POST",
     body: formData,
   })
   if (!res.ok) {
     throw new Error(`Server responded with ${res.status}`)
   }
-  return await res.text()
+  return response.text();
 }
